3fb2318cfc9b85e6d5e4fa0025c527b90d355ae7
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-09-09 07:36:20 +0700

    change function to waitForExist to avoid error in android emulator

--- specs/yt.search.spec.js
+++ specs/yt.search.spec.js
@@ -2,6 +2,6 @@ describe('Search Bar', function () {
   it('it should be able to input text and press button', function () {
     $('~searchInput').setValue('React Native');
     $('~searchButton').click();
-    $('~videoCard').waitForDisplayed(1000 * 20);
+    $('~videoCard').waitForExist(1000 * 60);
   });
 });
