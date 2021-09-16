08374d334e1ced5868c860fb9117a40b9885f3e9
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-09-17 02:14:33 +0700

    add video reporter, try headless emulator

--- yt-test.config.js
+++ yt-test.config.js
@@ -1,5 +1,5 @@
 const { join } = require('path');
-
+const video = require('wdio-video-reporter');
 exports.config = {
   runner: 'local',
   host: 'localhost',
@@ -15,6 +15,21 @@ exports.config = {
   },
   maxInstances: 1,
   sync: true,
+  reporters: [
+    [video, { saveAllVideos: true, videoSlowdownMultiplier: 10 }],
+    [
+      'allure',
+      {
+        outputDir: './__result__/allure',
+      },
+    ],
+    [
+      'json',
+      {
+        outputDir: './__result__/json',
+      },
+    ],
+  ],
   specs: ['./specs/**/yt*.spec.js'], // list of specs files
   capabilities: [
     {
@@ -23,9 +38,12 @@ exports.config = {
       // udid: '8AHY0LAXB', // check with adb devices
       udid: 'emulator-5554', //avd
       avd: 'pixel',
+      avdArgs: '-no-window',
       app: join(process.cwd(), './app/yt.apk'),
       appPackage: 'com.youtubesearchapi',
       appActivity: '.MainActivity',
+      newCommandTimeout: 1000 * 60,
+      isHeadless: true,
     },
   ],
 };
